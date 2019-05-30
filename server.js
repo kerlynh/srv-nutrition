5272cd4aaaaa79143f781e99cb9504f2164abc63
kerlynh <kerlynh@gmail.com>
2019-05-29 22:02:01 -0300

    Inserindo comidas do front no back

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const http = require('http');
 
+const comidas = {
+    pratosFavoritos: [
+  {
+    nome: "Batata frita",
+    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+    imagem: "img/Batata-frita.jpg"
+  },
+  {
+    nome: "Macarronada",
+    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+    imagem: "img/macarronada.jpg"
+  },
+  {
+    nome: "Falafel",
+    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+    imagem: "img/falafel.jpg"
+  },
+  {
+    nome: "Creme de abóbora",
+    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+    imagem: "img/creme-de-abobora.jpg"
+  }
+
+]}
+
 const servidor = http.createServer(function(request, response){
     if (request.url === '/') {
         response.end('Hello wonderful world!')
     } else if (request.url === '/comidas'){
-        // response.end('Resposta diferente')
     
         if (request.method === 'GET') {
-            response.writeHead(200, {"Content-Type": "text/html;charset=utf-8"})
-            response.end("<h1>Respostão diferentona</h1>")
+            response.writeHead(200, {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': "*"
+            })
+            response.write(JSON.stringify(comidas))
+            response.end()
         } else if (request.method === 'POST') {
-            response.writeHead(201, {"Content-Type": "text/html;charset=utf-8"})
+            response.writeHead(201, {
+                "Content-Type": "text/html;charset=utf-8"})
             response.end("<h1>Respostão diferentona do post</h1>")
         }
     }    
