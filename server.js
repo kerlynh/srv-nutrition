4e1d5def67afe1f01703accbef5a7178a4da599f
kerlynh <kerlynh@gmail.com>
2019-05-29 22:29:17 -0300

    Utilizando express

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const express = require('express');
 
 const comidas = {
     pratosFavoritos: [
@@ -25,26 +25,12 @@ const comidas = {
 
 ]}
 
-const servidor = http.createServer(function(request, response){
-    if (request.url === '/') {
-        response.end('Hello wonderful world!')
-    } else if (request.url === '/comidas'){
-    
-        if (request.method === 'GET') {
-            response.writeHead(200, {
-                "Content-Type": "application/json",
-                'Access-Control-Allow-Origin': "*"
-            })
-            response.write(JSON.stringify(comidas))
-            response.end()
-        } else if (request.method === 'POST') {
-            response.writeHead(201, {
-                "Content-Type": "text/html;charset=utf-8"})
-            response.end("<h1>Respostão diferentona do post</h1>")
-        }
-    }    
-})
+// ---- criando serivodr -----
+const servidor = express()
 
+servidor.get('/comidas', (request, response) => {
+  response.send('Boa noite, amigas!')
+})
 
 servidor.listen(3000)
 
