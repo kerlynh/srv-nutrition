acd400425cbf54b088b5c31ac3cfdce6f61de241
kerlynh <kerlynh@gmail.com>
2019-05-30 21:47:47 -0300

    Utilizando express e controller, cors

--- server.js
+++ server.js
@@ -1,37 +1,13 @@
-const express = require('express');
+const express = require('express')
+const cors = require('cors')
+const controller = require('./ComidasController')
 
-const comidas = {
-    pratosFavoritos: [
-  {
-    nome: "Batata frita",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/Batata-frita.jpg"
-  },
-  {
-    nome: "Macarronada",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/macarronada.jpg"
-  },
-  {
-    nome: "Falafel",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/falafel.jpg"
-  },
-  {
-    nome: "Creme de abóbora",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/creme-de-abobora.jpg"
-  }
-
-]}
-
-// ---- criando serivodr -----
 const servidor = express()
+servidor.use(cors())
 
 servidor.get('/comidas', (request, response) => {
-  response.send('Boa noite, amigas!')
+  response.send(controller.getAll())
 })
 
 servidor.listen(3000)
-
 console.log('servidorzinho rodando na porta 3000')
